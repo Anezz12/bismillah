3fe7a534baa3fae9b13da01dbed2805b0501f78b
Anezz12 <harsenaargrtya1@gmail.com>
2024-04-22 12:00:13 +0700

    update project

--- app/(home)/components/Project.tsx
+++ app/(home)/components/Project.tsx
@@ -35,13 +35,24 @@ function project() {
         text="Projects"
         className="flex flex-col items-center justify-center rotate-6 cursor-pointer"
       />
-      <div className=" grid grid-cols-1 sm:grid-cols-2 pt-20">
+
+      <div className=" grid grid-cols-1 sm:grid-cols-2 pt-20 gap-5">
         {projects.map((project, index) => {
           return (
             <Link href={project.link} key={index}>
               <div className={cn("p-5 rounded-md", project.background)}>
-                <DirectionAwareHover imageUrl={project.cover}>
-                  <h1>{project.title}</h1>
+                <DirectionAwareHover
+                  imageUrl={project.cover}
+                  className=" w-full space-y-5 cursor-pointer"
+                >
+                  <div className=" space-y-5">
+                    <h1 className=" text-2xl font-bold">{project.title}</h1>
+                    <div className="flex item-center gap-5">
+                      {project.tech.map((Icon, index) => {
+                        return <Icon className="w-7 h-7" key={index} />;
+                      })}
+                    </div>
+                  </div>
                 </DirectionAwareHover>
               </div>
             </Link>
